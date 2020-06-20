212ce18d69ccef16ccceef70aedc7cb331778db1
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-06-20 20:09:47 +0200

    webpack switch

---
+++ webpack.dev.config.js
@@ -0,0 +1,10 @@
+const merge = require("webpack-merge")
+const commonMod = require("./webpack.common.config")
+
+module.exports = (env) => {
+  const common = commonMod(env);
+  return merge(common, {
+    mode: "development",
+    watch: true
+  })
+};
